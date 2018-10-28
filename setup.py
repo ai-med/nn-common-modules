5cd339e5b5950e0513e14807c66b8fff667a2ef4
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-29 00:08:20 +0100

    inital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name="nn-common-modules",
+                 version="1.0",
+                 url="https://github.com/abhi4ssj/nn-common-modules",
+                 author="Shayan Ahmad Siddiqui",
+                 author_email="shayan.siddiqui89@gmail.com",
+                 description="Contains common modules which can be reused in a deep neural netwok specifically for segmentation",
+                 packages=setuptools.find_packages(),
+                 install_requires=['torch', 'squeeze-and-excite'])
