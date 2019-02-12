760783a7585715f2beb10dfaba6012aa41e0d5d7
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2019-02-12 17:14:28 +0100

    Adds nojekyll

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../nn_common_modules'))
+sys.path.insert(0, os.path.abspath('../../nn_common_modules'))
 
 
 # -- Project information -----------------------------------------------------
@@ -45,9 +45,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
