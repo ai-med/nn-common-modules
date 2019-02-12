90cfeeb68c57f469ed48f69f2f160e3c448d5157
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2019-02-12 16:26:29 +0100

    Updates losses and merges with this repo

---
+++ README.md
@@ -0,0 +1,41 @@
+# NN Additional Losses
+
+Common modules, blocks and losses which can be reused in a deep neural netwok specifically for segmentation Please use [technical documentation](https://shayansiddiqui.github.io/nn-additional-losses/build/html/) for a reference to API manual
+
+## Getting Started
+
+### Prerequisites
+
+You need to install pytorch in order for this work
+
+```
+pip install torch
+```
+
+### Installing
+
+This is not yet available on Pypi so you need to clone the repo in order to install
+
+Git clone
+
+```
+git clone https://github.com/shayansiddiqui/nn-common-modules
+```
+
+Then go to the cloned directory and install locally (Be sure of of environment where you are installing)
+
+```
+pip install -e .
+```
+
+Now the package is installed and ready to be used across all pytorch projects
+
+## Authors
+
+* **Shayan Ahmad Siddiqui**  - [shayansiddiqui](https://github.com/shayansiddiqui)
+* **Abhijit Guha Roy**  - [abhi4ssj](https://github.com/abhi4ssj)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ nn_common_modules/losses.py
@@ -0,0 +1,223 @@
+"""
+Addition losses module defines classses which are commonly used particularly in segmentation and are not part of standard pytorch library.
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.loss import _Loss, _WeightedLoss
+import numpy as np
+from torch.autograd import Variable
+
+
+class DiceLoss(_WeightedLoss):
+    """
+    Dice Loss for a batch of samples
+    """
+
+    def forward(self, output, target, weights=None, ignore_index=None, binary=False):
+        """
+        Forward pass
+
+        :param output: NxCxHxW logits
+        :param target: NxHxW LongTensor
+        :param weights: C FloatTensor
+        :param ignore_index: int index to ignore from loss
+        :param binary: bool for binarized one chaneel(C=1) input
+        :return:
+        """
+        output = F.softmax(output, dim=1)
+        if binary:
+            return self._dice_loss_binary(output, target)
+        return self._dice_loss_multichannel(output, target, weights, ignore_index)
+
+    @staticmethod
+    def _dice_loss_binary(output, target):
+        """
+        Dice loss for one channel binarized input
+
+        :param output: Nx1xHxW logits
+        :param target: NxHxW LongTensor
+        :return:
+        """
+        eps = 0.0001
+
+        intersection = output * target
+        numerator = 2 * intersection.sum(0).sum(1).sum(1)
+        denominator = output + target
+        denominator = denominator.sum(0).sum(1).sum(1) + eps
+        loss_per_channel = 1 - (numerator / denominator)
+
+        return loss_per_channel.sum() / output.size(1)
+
+    @staticmethod
+    def _dice_loss_multichannel(output, target, weights=None, ignore_index=None):
+        """
+        Forward pass
+
+        :param output: NxCxHxW Variable
+        :param target: NxHxW LongTensor
+        :param weights: C FloatTensor
+        :param ignore_index: int index to ignore from loss
+        :param binary: bool for binarized one chaneel(C=1) input
+        :return:
+        """
+        eps = 0.0001
+        encoded_target = output.detach() * 0
+
+        if ignore_index is not None:
+            mask = target == ignore_index
+            target = target.clone()
+            target[mask] = 0
+            encoded_target.scatter_(1, target.unsqueeze(1), 1)
+            mask = mask.unsqueeze(1).expand_as(encoded_target)
+            encoded_target[mask] = 0
+        else:
+            encoded_target.scatter_(1, target.unsqueeze(1), 1)
+
+        if weights is None:
+            weights = 1
+
+        intersection = output * encoded_target
+        numerator = 2 * intersection.sum(0).sum(1).sum(1)
+        denominator = output + encoded_target
+
+        if ignore_index is not None:
+            denominator[mask] = 0
+        denominator = denominator.sum(0).sum(1).sum(1) + eps
+        loss_per_channel = weights * (1 - (numerator / denominator))
+
+        return loss_per_channel.sum() / output.size(1)
+
+
+class IoULoss(_WeightedLoss):
+    """
+    IoU Loss for a batch of samples
+    """
+
+    def forward(self, output, target, weights=None, ignore_index=None):
+        """
+        Forward pass
+        :param output: NxCxHxW Variable
+        :param target: NxHxW LongTensor
+        :param weights: C FloatTensor
+        :param ignore_index: int index to ignore from loss
+        :return:
+        """
+        output = F.softmax(output, dim=1)
+
+        eps = 0.0001
+        encoded_target = output.detach() * 0
+
+        if ignore_index is not None:
+            mask = target == ignore_index
+            target = target.clone()
+            target[mask] = 0
+            encoded_target.scatter_(1, target.unsqueeze(1), 1)
+            mask = mask.unsqueeze(1).expand_as(encoded_target)
+            encoded_target[mask] = 0
+        else:
+            encoded_target.scatter_(1, target.unsqueeze(1), 1)
+
+        if weights is None:
+            weights = 1
+
+        intersection = output * encoded_target
+        numerator = intersection.sum(0).sum(1).sum(1)
+        denominator = (output + encoded_target) - (output*encoded_target)
+
+        if ignore_index is not None:
+            denominator[mask] = 0
+        denominator = denominator.sum(0).sum(1).sum(1) + eps
+        loss_per_channel = weights * (1 - (numerator / denominator))
+
+        return loss_per_channel.sum() / output.size(1)
+
+
+class CrossEntropyLoss2d(_WeightedLoss):
+    """
+    Standard pytorch weighted nn.CrossEntropyLoss
+    """
+
+    def __init__(self, weight=None):
+        super(CrossEntropyLoss2d, self).__init__()
+        self.nll_loss = nn.CrossEntropyLoss(weight)
+
+    def forward(self, inputs, targets):
+        """
+        Forward pass
+
+        :param inputs: torch.tensor (NxC)
+        :param targets: torch.tensor (N)
+        :return: scalar
+        """
+        return self.nll_loss(inputs, targets)
+
+
+class CombinedLoss(_Loss):
+    """
+    A combination of dice  and cross entropy loss
+    """
+
+    def __init__(self):
+        super(CombinedLoss, self).__init__()
+        self.cross_entropy_loss = CrossEntropyLoss2d()
+        self.dice_loss = DiceLoss()
+        self.focal_loss = FocalLoss()
+        self.l2_loss = nn.MSELoss()
+
+    def forward(self, input, target, weight=None):
+        """
+        Forward pass
+
+        :param input: torch.tensor (NxCxHxW)
+        :param target: torch.tensor (NxHxW)
+        :param weight: torch.tensor (NxHxW)
+        :return: scalar
+        """
+        input_soft = F.softmax(input, dim=1)
+        y_2 = torch.mean(self.dice_loss(input_soft, target))
+        if weight is None:
+            y_1 = torch.mean(self.cross_entropy_loss.forward(input, target))
+        else:
+            y_1 = torch.mean(
+                torch.mul(self.cross_entropy_loss.forward(input, target), weight.cuda()))
+        return y_1 + y_2
+
+
+# Credit to https://github.com/clcarwin/focal_loss_pytorch
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2, alpha=None, size_average=True):
+
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        if isinstance(alpha, (float, int)):
+            self.alpha = torch.Tensor([alpha, 1-alpha])
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim() > 2:
+            # N,C,H,W => N,C,H*W
+            input = input.view(input.size(0), input.size(1), -1)
+            input = input.transpose(1, 2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1, input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1, 1)
+
+        logpt = F.log_softmax(input, dim=1)
+        logpt = logpt.gather(1, target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type() != input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0, target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.sum()
